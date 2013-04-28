46e5a0ab3180222bf584450378dd22e224c15718
cb <cb@cb.com>
2013-04-28 15:32:27 +0800

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 cb-navigation
 =============
+This framework provide a common navigation frame with the MVVM pattern
+It depends on Jquery
+To used it:
+1. Inlude JQuery
+2. Include cb-ko-helper.js and knockout js
+3. Include cb-navigation.js
+
+4. Create the mamin view (html/mvc ... and so on)
+   in this view, you should init the navigation service as below:
+   var navigationService = new NavigationService();
+   navigationService.init('#mainContentContainer');
+
+5. Create the one view for the navigation, for example, the new view url is http://***.**.com/views/view1
+   in this view, you should create a script block like below:
+   <script type="text/javascript" id="homeMainViewScript">
+		(function () {
+			**** same js code ***
+			NavigationService.attachViewModel("#homeMainViewScript", new HomeMainViewModel(new Backbone.Model({})));
+		})();
+   </script>
+   1): the script should have a id
+   2): in the code, you should call the menthod NavigationService.attachViewModel, this method will let the navigation find you viewmodel when it navigate to the view. the first parameter is the script selector and the second parament is the viewmmodel instance
+
+6. If you need to navigate to the the view you created in steps 5, use the code below:
+   navigationService.navigateTo('http://***.**.com/views/view1')
+
+ Methods:
+ navigateTo: (url, [requestNew,] [viewModel,] [viewHtml])
+ the url is the key of each view, and navigate servie us it to find the view. for the default, requestNew is false, viewModel and viewHtml are both empty
+ when the navigate service find the request is true or can't find the view from cache, it will try to call this url to get the view html and then evalate the html to get the viewModel (if viewModel is empty)
+ then use ko to apply the binding to the view and then show the view with default animation.
+
+ if you want to manually navigate to an existing view, then specify the viewHtml with the html.
+ if you want change the default animation of view transform, you can overwrite NavigationServiceAnimationManager.toAnimation and fromAnimation
---
+++ cb-navigation.js
@@ -0,0 +1,204 @@
+﻿//Dependency: Jquery, Underscore
+//Define the NavigationService class
+var NavigationService = function (navigateContainer) {
+    var _ = {
+        isUndefined: function (obj) {
+            return typeof (obj) === "undefined";
+        }
+    };
+
+    var self = this;
+    var $Class = NavigationService;
+    //constants
+    $Class.VIEWMODELONACTIVEEVENT = "onActive";
+    $Class.VIEWMODELONINACTIVEEVENT = "onInactive";
+    $Class.SCRIPTVIEWMODELPROPERTY = "viewModel";
+
+    //Static Method
+    $Class.attachViewModel = function (id, viewModel) {
+        if ($(id).length <= 0) {
+            throw "Can NOT find " + id;
+        }
+        if ($(id).prop('tagName') !== "SCRIPT") {
+            throw "only script element can be attached";
+        }
+        $(id)[0][$Class.SCRIPTVIEWMODELPROPERTY] = viewModel;
+    };
+
+    //Private Fields
+    self.internalViewsViewModelsCache = new Array(); //{url:***, viewModel:***, viewHtml:***}
+    self.currentActiveContent = undefined;
+
+    self.init = function (navContainer) {
+        if (!_.isUndefined(navContainer)) {
+            navigateContainer = $(navContainer);
+        }
+        if (!_.isUndefined(navigateContainer)) {
+            navigateContainer = $(navigateContainer);
+        }
+        self.internalViewsViewModelsCache = new Array();
+        self.currentActiveContent = undefined;
+    };
+
+    self.init(navigateContainer);
+
+    var doNavigation = function (viewViewModelObj) {
+        var continueFunc = function () {
+            //navigateContainer.empty();
+
+            var contentObj = _.find(self.internalViewsViewModelsCache, function (item) {
+                return item.url === viewViewModelObj.url;
+            });
+            if (_.isUndefined(contentObj)) {
+                contentObj = viewViewModelObj;
+                self.internalViewsViewModelsCache.push(contentObj);
+            } else {
+                $.extend(contentObj, viewViewModelObj);
+            }
+
+            //navigateContainer.html(contentObj.viewHtml);
+            var tmpContainerForNew = $("<div/>");
+            navigateContainer.after(tmpContainerForNew);
+            tmpContainerForNew.hide();
+            tmpContainerForNew.html(contentObj.viewHtml);
+            if (_.isUndefined(contentObj.viewModel)) {
+                var script = _.find(tmpContainerForNew.children("script"), function (s) {
+                    return !_.isUndefined(s[$Class.SCRIPTVIEWMODELPROPERTY]);
+                });
+                if (!_.isUndefined(script)) {
+                    contentObj.viewModel = script[$Class.SCRIPTVIEWMODELPROPERTY];
+                }
+            }
+            self.currentActiveContent = contentObj;
+
+            function afterActived() {
+                ko.applyBindings(contentObj.viewModel, tmpContainerForNew[0]);
+
+                function showNewContent() {
+                    navigateContainer.empty();
+                    //tmpContainerForNew.show();
+                    navigateContainer.append(tmpContainerForNew);
+
+                    function afterAnimation() {
+                        //forec to refresh, because I don't know why somt time, the content is not auto refreshed.
+                        if (ko.$helper && ko.$helper.browser.isIE && ko.$helper.browser.version == 10) {
+                            setTimeout(function () {
+                                navigateContainer.focus();
+                                var tmpHeight = navigateContainer.height();
+                                navigateContainer.height(tmpHeight + 1);
+                                navigateContainer.height(tmpHeight);
+                                navigateContainer.height("auto");
+                            }, 500);
+                        }
+                    }
+
+                    NavigationServiceAnimationManager.doNavigateToAnimation(tmpContainerForNew, afterAnimation);
+                }
+
+                NavigationServiceAnimationManager.doNavigateFromAnimation(navigateContainer.children(), showNewContent);
+            }
+
+
+            if (!_.isUndefined(self.currentActiveContent) && !_.isUndefined(self.currentActiveContent.viewModel) && !_.isUndefined(self.currentActiveContent.viewModel[$Class.VIEWMODELONACTIVEEVENT])) {
+                self.currentActiveContent.viewModel[$Class.VIEWMODELONACTIVEEVENT](afterActived);
+            } else {
+                afterActived();
+            }
+        };
+
+
+        if (!_.isUndefined(self.currentActiveContent) && !_.isUndefined(self.currentActiveContent.viewModel) && !_.isUndefined(self.currentActiveContent.viewModel[$Class.VIEWMODELONINACTIVEEVENT])) {
+            self.currentActiveContent.viewModel[$Class.VIEWMODELONINACTIVEEVENT](continueFunc);
+        } else {
+            continueFunc();
+        }
+    };
+
+    var navigateToEx = function (requestObj, requestNew, viewModel, viewHtml) {
+        if (requestNew && !_.isUndefined(viewHtml)) {
+            throw "When requestNew is true, can't specify viewHtml";
+        }
+        var vvm = _.find(self.internalViewsViewModelsCache, function (item) {
+            return item.url === requestObj.url;
+        });
+        if (_.isUndefined(vvm)) {
+            vvm = {};
+        }
+
+        if (requestNew || _.isUndefined(vvm.viewHtml)) {
+            requestObj.request(function (data) {
+                if (!_.isUndefined(data.status)) {
+                    throw data.responseText;
+                }
+                $.extend(vvm, { url: requestObj.url, viewModel: viewModel, viewHtml: data });
+                doNavigation(vvm);
+            });
+        } else {
+            $.extend(vvm, { url: requestObj.url, viewModel: viewModel, viewHtml: viewHtml });
+            doNavigation(vvm);
+        }
+    };
+
+    //url [, viewModel, viewHtml]
+    //url[, true, viewModel]
+    self.navigateTo = function (url, requestNew, viewModel, viewHtml) {
+        if (_.isUndefined(requestNew)) {
+            requestNew = false;
+        }
+
+        if ($.type(requestNew) !== "boolean") {
+            viewHtml = viewModel;
+            viewModel = requestNew;
+            requestNew = _.isUndefined(viewHtml);
+        }
+
+        var requestObj = url;
+        if ($.type(url) === "string") {
+            requestObj = {
+                request: function (callBack) {
+                    $.ajax({
+                        url: url
+                    }).done(callBack).fail(callBack);
+                },
+                url: url
+            };
+        }
+
+        navigateToEx(requestObj, requestNew, viewModel, viewHtml);
+    };
+
+};
+
+var NavigationServiceAnimationConst = {
+    TransformAnimationAttr: "transformAnimation",
+    NoTransformAnimationTag: "NoTransformAnimation"
+};
+
+var NavigationServiceAnimationManager = {
+    doNavigateToAnimation: function (navToContent, finishedCallback) {
+        navToContent = $(navToContent);
+        if (navToContent.filter("[" + NavigationServiceAnimationConst.TransformAnimationAttr + "='" + NavigationServiceAnimationConst.NoTransformAnimationTag + "']").length > 0) {
+            navToContent.show();
+            finishedCallback();
+        } else {
+            NavigationServiceAnimationManager.toAnimation(navToContent, finishedCallback);
+        }
+    },
+
+    doNavigateFromAnimation: function (navFromContent, finishedCallback) {
+        navFromContent = $(navFromContent);
+        if (navFromContent.length > 0) {
+            NavigationServiceAnimationManager.toAnimation(navFromContent, finishedCallback);
+        } else {
+            finishedCallback();
+        }
+    },
+
+    toAnimation: function (content, finishedCallback) {
+        content.slideDown(finishedCallback);
+    },
+
+    fromAnimation: function (content, finishedCallback) {
+        content.slideUp(finishedCallback);
+    }
+};
