bf01077d610462e922d285be6e589c56f7b96e07
cb <cb@cb.com>
2014-07-15 14:25:17 +0800

    Add typescript typeing file

---
+++ cb-navigation.d.ts
@@ -0,0 +1,30 @@
+﻿declare module $CB.Navigation {
+    interface IViewModel {
+        //method for navigation
+        onActive?(continueCallback: () => void): void;
+        onInactive?(continueCallback: () => void): void;
+        OnViewInit?(): void;
+        OnViewUnload?(): void;
+    }
+
+    interface INavigationService {
+        init(): void;
+        init(navContainer: string): void;
+        init(navContainer: JQueryStatic): void;
+        navigateTo(url: string, requestNew: boolean, viewModel: IViewModel, viewHtml: string, navigatedCallBack: () => void): void;
+        navigateTo(url: string, viewModel?: IViewModel, viewHtml?: string): void;
+        navigateTo(url: string, navigatedCallBack: () => void): void;
+    }
+
+    interface NavigationServiceAnimationConst {
+        TransformAnimationAttr: string;
+        NoTransformAnimationTag: string;
+    }
+
+    interface NavigationServiceAnimationManager {
+        doNavigateToAnimation(navToContent, finishedCallback: () => void): void;
+        doNavigateFromAnimation(navFromContent, finishedCallback: () => void): void;
+        toAnimation(content, finishedCallback: () => void): void;
+        fromAnimation(content, finishedCallback: () => void): void;
+    }
+}
