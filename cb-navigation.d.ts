595cb8661ec432fd0e8290dbb4fa9cac0d0577fa
cb <cb@cb.com>
2014-08-01 07:39:40 +0800

    update navgiation d.ts

--- cb-navigation.d.ts
+++ cb-navigation.d.ts
@@ -1,10 +1,10 @@
 ﻿declare module $CB.Navigation {
     interface IViewModel {
         //method for navigation
-        onActive?(continueCallback: () => void): void;
-        onInactive?(continueCallback: () => void): void;
-        OnViewInit?(): void;
-        OnViewUnload?(): void;
+        onActive? (continueCallback: () => void): void;
+        onInactive? (continueCallback: () => void): void;
+        OnViewInit? (): void;
+        OnViewUnload? (): void;
     }
 
     interface INavigationService {
@@ -16,15 +16,39 @@
         navigateTo(url: string, navigatedCallBack: () => void): void;
     }
 
-    interface NavigationServiceAnimationConst {
+    interface INavigationServiceAnimationConst {
         TransformAnimationAttr: string;
         NoTransformAnimationTag: string;
     }
 
-    interface NavigationServiceAnimationManager {
+    interface INavigationServiceAnimationManager {
         doNavigateToAnimation(navToContent, finishedCallback: () => void): void;
         doNavigateFromAnimation(navFromContent, finishedCallback: () => void): void;
         toAnimation(content, finishedCallback: () => void): void;
         fromAnimation(content, finishedCallback: () => void): void;
     }
+
+    interface INavigationOption {
+        onParseContentHtmlException? (exception): void;
+        onBeforeDoNavigate? (): void;
+        onEndNavigate? (continueActive: boolean): void;
+        validateHttpResponse? (data, textStatus, jqXHR: JQueryXHR): void;
+        onNavigationHttpRequestError? (jqXHR: JQueryXHR, textStatus, errorThrown): void;
+    }
 }
+
+declare class NavigationService implements $CB.Navigation.INavigationService {
+    init(): void;
+    init(navContainer: string): void;
+    init(navContainer: JQueryStatic): void;
+    navigateTo(url: string, requestNew: boolean, viewModel: $CB.Navigation.IViewModel, viewHtml: string, navigatedCallBack: () => void): void;
+    navigateTo(url: string, viewModel?: $CB.Navigation.IViewModel, viewHtml?: string): void;
+    navigateTo(url: string, navigatedCallBack: () => void): void;
+
+    constructor(navigateContainer: string, option?: $CB.Navigation.INavigationOption);
+    constructor(navigateContainer: JQueryStatic, option?: $CB.Navigation.INavigationOption);
+    //public static 
+}
+
+declare var NavigationServiceAnimationManager: $CB.Navigation.INavigationServiceAnimationManager;
+declare var NavigationServiceAnimationConst: $CB.Navigation.INavigationServiceAnimationConst;
